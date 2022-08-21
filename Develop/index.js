99b76cebc25f3abe28b7d6858219df237ea3caa9
Coding Padawan <gustavohiguera9715@gmail.com>
2022-08-21 16:49:02 -0700

    working on rendering markdown to 2nd js file

--- Develop/index.js
+++ Develop/index.js
@@ -3,56 +3,63 @@ const inquirer = require('inquirer');
 const fs = require('fs')
 const mdFile = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your first and last name?'
+    },
+    {
+      type: 'input',
+      name: 'username',
+      message: 'What is your Github username?'
+    },
+    {
+      type: 'input',
+      name: 'linkedin',
+      message: 'What is your LinkedIn URL?'
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your email address?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a description of your project.'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Please provide the installation instructions of your project if there are any.'
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Which license would you like to use for your project?',
+      choices: ['MIT', 'ISC', 'GNU GPLv3', 'Apache 2.0']
+    },
+  ])
 
-// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-// HEN I choose a license for my application from a list of options
-inquirer.prompt([
-  {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your project?'
-  },
-  {
-    type: 'input',
-    name: 'username',
-    message: 'What is your Github username?'
-  },
-  {
-    type: 'input',
-    name: 'email',
-    message: 'What is your email address?'
-  },
-  {
-    type: 'input',
-    name: 'description',
-    message: 'Please provide a description of your project.'
-  },
-  {
-    type: 'input',
-    name: 'installation',
-    message: 'Please provide the installation instructions of your project if there are any.'
-  },
-  {
-    type: 'list',
-    name: 'license',
-    message: 'Which license would you like to use for your project?',
-    choices: ['MIT', 'ISC', 'GNU GPLv3', 'Apache 2.0']
-  },
-])
+    .then((data) => {
+      mdFile.generateMarkdown(data);
+      console.log(data)
+    })
+}
 
-  .then((data) => {
-    mdFile.generateMarkdown(data);
-  })
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, markdownContent, (err) =>
+    err ? console.log(err) : console.log('Success!'))
+}
 
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//   fs.writeFile(fileName, data, (err) =>
-//     err ? console.log(err) : console.log('Success!'))
-// }
-
-// // TODO: Create a function to initialize app
-// function init() { }
-
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,44 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) { }
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+      "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+      break;
+    case "ISC":
+      "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+      break;
+    case "GNU GPLv3":
+      "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+      break;
+    case "Apache 2.0":
+      "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+      break;
+    default:
+      "";
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) { }
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      "https://choosealicense.com/licenses/mit/"
+      break;
+    case "ISC":
+      "https://choosealicense.com/licenses/isc/"
+      break;
+    case "GNU GPLv3":
+      "https://choosealicense.com/licenses/gpl-3.0/"
+      break;
+    case "Apache 2.0":
+      "https://choosealicense.com/licenses/apache-2.0/"
+      break;
+    default:
+      "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,11 +47,32 @@ function renderLicenseSection(license) { }
 // TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
   const fileName = 'README.md';
+
   writeToFile(fileName, data);
 
-  return `# ${data.title}
+  return `
+  #**${data.title}**
 
-`;
+  ## License
+
+  ${data.license}
+
+  ## Author
+  
+  **${data.name}**
+  * GitHub: https://github.com/${data.username}
+  * LinkedIn: ${data.linkedin}
+  * Email: ${data.email}
+  
+  ## Description
+
+  ${data.description}
+
+  ## Installation:
+
+  ${data.installation}
+  
+`
 }
 
-module.exports = generateMarkdown;
+module.exports = generateMarkdown();
